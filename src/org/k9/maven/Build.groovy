ad707bdb36e31bf44543b160472630bf9f4a7155
srinivasdevops376 <35996741+srinivasdevops376@users.noreply.github.com>
2018-03-15 11:21:00 +0530

    Create Build.groovy

---
+++ src/org/k9/maven/Build.groovy
@@ -0,0 +1,23 @@
+package org.k9.git
+import org.k9.*
+
+class Scm implements Serializable {
+  def config
+  def script
+
+  Scm(script,config) {
+    this.config = config
+    this.script = script
+  }
+  
+  void checkout(){    
+    this.script.stage('checkout'){
+        this.script.git(url: this.config.scm_url, branch: this.config.scm_branch)
+
+    }
+  
+    }
+    void mavenBuild(){
+         this.script.sh "mvn -Dmaven.test.failure.ignore clean package"
+    }
+  }
